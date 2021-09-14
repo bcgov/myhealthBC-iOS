fabbb2489edaab3e31f2177c9b5ad79c980fca1c
ConnorOgilvie <connor@freshworks.io>
2021-09-14 11:49:30 -0700

    Initial Xcode project commit

---
+++ BCVaccineCard/BCVaccineCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2021-09-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
