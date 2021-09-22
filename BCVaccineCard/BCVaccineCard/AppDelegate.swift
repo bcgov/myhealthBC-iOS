341877587d26dc37203c6ae91021746beab99480
ConnorOgilvie <connor@freshworks.io>
2021-09-22 15:32:43 -0700

    App delegate updated for scene delegate

--- BCVaccineCard/BCVaccineCard/AppDelegate.swift
+++ BCVaccineCard/BCVaccineCard/AppDelegate.swift
@@ -13,6 +13,8 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         BCVaccineValidator.testMode = true
