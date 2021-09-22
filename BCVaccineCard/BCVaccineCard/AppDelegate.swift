7744f319033c5e967a98f4df42d06d076d1fb291
ConnorOgilvie <connor@freshworks.io>
2021-09-22 15:11:21 -0700

    Provisioning profiles added

--- BCVaccineCard/BCVaccineCard/AppDelegate.swift
+++ BCVaccineCard/BCVaccineCard/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 import BCVaccineValidator
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         BCVaccineValidator.testMode = true
+        FirebaseApp.configure()
         return true
     }
 
