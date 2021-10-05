73817b6bd9cc61ca68408bf2be6c189efef7e035
ConnorOgilvie <connor@freshworks.io>
2021-10-05 09:42:14 -0700

    Storyboards created for future features

--- BCVaccineCard/BCVaccineCard/Utility/Storyboard.swift
+++ BCVaccineCard/BCVaccineCard/Utility/Storyboard.swift
@@ -10,4 +10,8 @@ import UIKit
 enum Storyboard {
     static var main: UIStoryboard { return UIStoryboard(name: "Main", bundle: nil) }
     static var healthPass: UIStoryboard { return UIStoryboard(name: "HealthPass", bundle: nil) }
+    static var records: UIStoryboard { return UIStoryboard(name: "Records", bundle: nil) }
+    static var checker: UIStoryboard { return UIStoryboard(name: "Checker", bundle: nil) }
+    static var booking: UIStoryboard { return UIStoryboard(name: "Booking", bundle: nil) }
+    static var notifications: UIStoryboard { return UIStoryboard(name: "Notifications", bundle: nil) }
 }
