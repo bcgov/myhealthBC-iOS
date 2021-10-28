d6469219551c8841f363ef791dcbb82bae6a2339
Amir Shayegh <shayegh@me.com>
2021-10-28 10:00:11 -0700

    cleanup

--- BCVaccineCard/BCVaccineCard/Helpers/Extensions/Foundation/Array+EXT.swift
+++ BCVaccineCard/BCVaccineCard/Helpers/Extensions/Foundation/Array+EXT.swift
@@ -7,18 +7,14 @@
 
 import Foundation
 
-extension Array where Element: Equatable
-{
+extension Array where Element: Equatable {
     mutating func move(_ element: Element, to newIndex: Index) {
         if let oldIndex: Int = self.firstIndex(of: element) { self.move(from: oldIndex, to: newIndex) }
     }
 }
 
-extension Array
-{
+extension Array {
     mutating func move(from oldIndex: Index, to newIndex: Index) {
-        // Don't work for free and use swap when indices are next to each other - this
-        // won't rebuild array and will be super efficient.
         if oldIndex == newIndex { return }
         if abs(newIndex - oldIndex) == 1 { return self.swapAt(oldIndex, newIndex) }
         self.insert(self.remove(at: oldIndex), at: newIndex)
