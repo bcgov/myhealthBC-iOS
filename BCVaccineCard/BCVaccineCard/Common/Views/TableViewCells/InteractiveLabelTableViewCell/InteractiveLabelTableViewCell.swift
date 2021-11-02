7ba949788b5e3ad9b23ff5877c439dda6a5c6c2c
ConnorOgilvie <connor@freshworks.io>
2021-11-01 20:31:24 -0700

    Added clickable label

---
+++ BCVaccineCard/BCVaccineCard/Common/Views/TableViewCells/InteractiveLabelTableViewCell/InteractiveLabelTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  InteractiveLabelTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2021-11-01.
+//
+
+import UIKit
+
+class InteractiveLabelTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var clickableLabel: InteractiveLinkLabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    private func setup() {
+        
+    }
+    
+    func configure() {
+        
+    }
+    
+}
