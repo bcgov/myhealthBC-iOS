f203410d67266f6da06d2c5774b14c511a0aa855
ConnorOgilvie <connor@freshworks.io>
2021-11-05 16:25:54 -0700

    deleted commented code

--- BCVaccineCard/BCVaccineCard/Models/LocalModels/FormDataSource.swift
+++ BCVaccineCard/BCVaccineCard/Models/LocalModels/FormDataSource.swift
@@ -7,54 +7,6 @@
 
 import UIKit
 
-// MARK: This model is for the table view data source of the gateway screen
-//struct FormDataSource: Equatable {
-//    enum SpecificCell {
-//        case introText
-//        case phnForm
-//        case dobForm
-//        case dovForm
-//        case rememberCheckbox
-//        case clickablePrivacyPolicy
-//    }
-//    enum CellType: Equatable {
-//        case text(type: TextCellType, font: UIFont)
-//        case form(type: FormTextFieldType)
-//        case checkbox(text: String)
-//        case clickableText(text: String, linkedStrings: [LinkedStrings])
-//    }
-//
-//    let type: CellType
-//    var cellStringData: String?
-//    let specificCell: SpecificCell
-//
-//    func isTextField() -> Bool {
-//        switch type {
-//        case .text: return false
-//        case .form: return true
-//        case .checkbox: return false
-//        case .clickableText: return false
-//        }
-//    }
-//
-//    func transform() -> TextFieldData? {
-//        switch self.type {
-//        case .text: return nil
-//        case .form(type: let type):
-//            return TextFieldData(type: type, text: self.cellStringData)
-//        case .checkbox: return nil
-//        case .clickableText: return nil
-//        }
-//    }
-//}
-
-//struct TextFieldData: Equatable {
-//    let type: FormTextFieldType
-//    let text: String?
-//}
-
-
-
 struct FormData: Equatable {
     struct Configuration: Equatable {
         var text: String? = nil
