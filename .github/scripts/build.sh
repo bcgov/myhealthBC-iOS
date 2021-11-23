05381d19d90ea919e1c3357348d64388bf226038
Amir Shayegh <shayegh@me.com>
2021-11-23 09:48:20 -0800

    cleanup

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 sudo xcodebuild -workspace BCVaccineCard/BCVaccineCard.xcworkspace \
 			-scheme BCVaccineCard \
 			-sdk iphonesimulator \
-			-archivePath $PWD/build/BCVaccineCard-Prod.xcarchive \
+			-archivePath $PWD/build/BCVaccineCard.xcarchive \
 			clean build | xcpretty
