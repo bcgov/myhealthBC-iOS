445f3f19cde42a94be3402f18e00042d7356cb48
ConnorOgilvie <connor@freshworks.io>
2021-11-29 12:09:12 -0800

    Amir status banner view added Table View cell placeholder added which will contain status banner view

---
+++ BCVaccineCard/BCVaccineCard/Common/Views/HealthRecordsViews/TableViewCells/BannerViewTableViewCell/BannerViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BannerViewTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2021-11-29.
+//
+
+import UIKit
+
+class BannerViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
