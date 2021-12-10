a9f449cfdd015f170073cf435f2a10cf3949b98b
ConnorOgilvie <connor@freshworks.io>
2021-12-10 11:15:53 -0800

    first commit after losing all work

---
+++ BCVaccineCard/BCVaccineCard/NetworkLayer/APIClient.swift
@@ -0,0 +1,56 @@
+//
+//  HealthGatewayBCWorker.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2021-10-10.
+//
+
+import UIKit
+
+class APIClient {
+    
+    private var delegateOwner: UIViewController
+        
+    init(delegateOwner: UIViewController) {
+        self.delegateOwner = delegateOwner
+    }
+    
+    private var endpoints: EndpointsAccessor {
+       return UrlAccessor()
+    }
+    
+    private var remote: RemoteAccessor {
+        return NetworkAccessor()
+    }
+    
+    func getVaccineCard(_ model: GatewayVaccineCardRequest, token: String?, executingVC: UIViewController, completion: @escaping NetworkRequestCompletion<GatewayVaccineCardResponse>) {
+        let interceptor = NetworkRequestInterceptor()
+        let url = configureURL(token: token, endpoint: self.endpoints.getVaccineCard)
+        
+        let headerParameters: Headers = [
+            Constants.GatewayVaccineCardRequestParameters.phn: model.phn,
+            Constants.GatewayVaccineCardRequestParameters.dateOfBirth: model.dateOfBirth,
+            Constants.GatewayVaccineCardRequestParameters.dateOfVaccine: model.dateOfVaccine
+        ]
+        
+        guard let unwrappedURL = url else { return }
+        self.remote.request(withURL: unwrappedURL, method: .get, headers: headerParameters, interceptor: interceptor, checkQueueIt: true, executingVC: executingVC, andCompletion: completion)
+    }
+}
+
+// MARK: QUEUEIT Logic here
+extension APIClient {
+    
+    func configureURL(token: String?, endpoint: URL) -> URL? {
+        var url: URL?
+        if let token = token {
+            let queryItems = [URLQueryItem(name: Constants.QueueItStrings.queueittoken, value: token)]
+            var urlComps = URLComponents(string: endpoint.absoluteString)
+            urlComps?.queryItems = queryItems
+            url = urlComps?.url
+        } else {
+            url = endpoint
+        }
+        return url
+    }
+}
