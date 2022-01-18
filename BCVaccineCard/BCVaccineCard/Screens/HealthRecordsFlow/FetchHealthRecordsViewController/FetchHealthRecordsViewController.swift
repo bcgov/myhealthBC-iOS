00fb420e83ba560b66c7578f37d1c6adb200cd16
ConnorOgilvie <connor@freshworks.io>
2022-01-18 10:21:33 -0800

    pulled from dev, removed outdated comment

--- BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/FetchHealthRecordsViewController/FetchHealthRecordsViewController.swift
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/FetchHealthRecordsViewController/FetchHealthRecordsViewController.swift
@@ -3,7 +3,6 @@
 //  BCVaccineCard
 //
 //  Created by Connor Ogilvie on 2021-11-29.
-// TODO: This will contain a table view with the two cells for immunization record and test results. This view controller will be shown automatically on top of the healthRecordsViewController when no health records exist - will have to show a loading indicator
 
 import UIKit
 
