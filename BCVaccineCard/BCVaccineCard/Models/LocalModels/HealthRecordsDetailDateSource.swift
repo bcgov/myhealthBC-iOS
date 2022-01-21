3ead839b23709bed11799b386b644c85fc776931
ConnorOgilvie <connor@freshworks.io>
2022-01-21 13:43:16 -0800

    updated links functionality for result description

--- BCVaccineCard/BCVaccineCard/Models/LocalModels/HealthRecordsDetailDateSource.swift
+++ BCVaccineCard/BCVaccineCard/Models/LocalModels/HealthRecordsDetailDateSource.swift
@@ -124,28 +124,48 @@ struct HealthRecordsDetailDataSource {
             TextListModel(header: TextListModel.TextProperties(text: "Provider / Clinic:", bolded: true), subtext: TextListModel.TextProperties(text: testResult.lab ?? "", bolded: false))
         ])
         if let resultDescription = testResult.resultDescription, !resultDescription.isEmpty {
-            var descriptionString = ""
-            for (index, description) in resultDescription.enumerated() {
-                descriptionString.append(description)
-                if index < resultDescription.count - 1 {
-                    descriptionString.append("\n\n")
-                }
-            }
-            if descriptionString.last != " " {
-                descriptionString.append(" ")
-            }
-            var linkedStrings: [LinkedStrings]?
-            if let link = testResult.resultLink, !link.isEmpty {
-                let text = "this page"
-                descriptionString.append(text)
-                let linkedString = LinkedStrings(text: text, link: link)
-                linkedStrings = []
-                linkedStrings?.append(linkedString)
-            }
-            let resultDescriptionfield = TextListModel(header: TextListModel.TextProperties(text: "Result description:", bolded: true), subtext: TextListModel.TextProperties(text: descriptionString, bolded: false, links: linkedStrings))
+            let tuple = self.handleResultDescriptionAndLinks(resultDescription: resultDescription, testResult: testResult)
+            let resultDescriptionfield = TextListModel(header: TextListModel.TextProperties(text: "Result description:", bolded: true), subtext: TextListModel.TextProperties(text: tuple.text, bolded: false, links: tuple.links))
             fields[0].append(resultDescriptionfield)
         }
         return Record(id: testResult.id ?? UUID().uuidString, name: testResult.patientDisplayName ?? "", type: .covidTestResultRecord(model: testResult), status: status, date: date, fields: fields)
+    }
+    
+    // Note this funcion is used to append "this page" text with link from API to end of result description. In the event where there is a positive test, there is no link, but there are links embedded in the text. For this, we use NSDataDetector to create links
+    private static func handleResultDescriptionAndLinks(resultDescription: [String], testResult: TestResult) -> (text: String, links: [LinkedStrings]?) {
+        var descriptionString = ""
+        for (index, description) in resultDescription.enumerated() {
+            descriptionString.append(description)
+            if index < resultDescription.count - 1 {
+                descriptionString.append("\n\n")
+            }
+        }
+        if descriptionString.last != " " {
+            descriptionString.append(" ")
+        }
+        var linkedStrings: [LinkedStrings]?
+        if let link = testResult.resultLink, !link.isEmpty {
+            let text = "this page"
+            descriptionString.append(text)
+            let linkedString = LinkedStrings(text: text, link: link)
+            linkedStrings = []
+            linkedStrings?.append(linkedString)
+        }
+        do {
+            let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+            guard let matches = detector?.matches(in: descriptionString, options: [], range: NSRange(location: 0, length: descriptionString.utf16.count)) else { return (descriptionString, linkedStrings) }
+            for match in matches {
+                guard let range = Range(match.range, in: descriptionString) else { continue }
+                let url = descriptionString[range]
+                let linkString = String(url)
+                let newLink = LinkedStrings(text: linkString, link: linkString)
+                if linkedStrings == nil {
+                    linkedStrings = []
+                }
+                linkedStrings?.append(newLink)
+            }
+        }
+        return (descriptionString, linkedStrings)
     }
 }
 
