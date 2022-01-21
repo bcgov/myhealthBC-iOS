85cd6a2da3793119c62240dc503ab6de09bc4bc2
ConnorOgilvie <connor@freshworks.io>
2022-01-21 11:45:56 -0800

    removed old text

--- BCVaccineCard/BCVaccineCard/Common/Views/HealthRecordsViews/UserRecordListView/UserRecordListView.swift
+++ BCVaccineCard/BCVaccineCard/Common/Views/HealthRecordsViews/UserRecordListView/UserRecordListView.swift
@@ -3,7 +3,7 @@
 //  BCVaccineCard
 //
 //  Created by Connor Ogilvie on 2021-11-10.
-// https://www.figma.com/file/ga1F6q5Kvi6CD6FLS27fXq/My-Health-BC?node-id=2411%3A24372
+
 // NOTE: This is for an individual's
 import UIKit
 
