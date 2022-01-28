b2a2143b38131ac96d39e35918c0aaefa7d18bd9
Mohamed Fawzy <mfawzy@freshworks.io>
2022-01-28 18:31:02 +0200

    update CoreDataStack

--- BCVaccineCard/BCVaccineCard/CoreDataStack.swift
+++ BCVaccineCard/BCVaccineCard/CoreDataStack.swift
@@ -17,33 +17,20 @@ extension CoreDataStackProtocol {
     var managedContext: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
-    
-    public func saveContext() {
-        saveContext(managedContext)
-    }
 
-    public func saveContext(_ context: NSManagedObjectContext) {
-        if context != managedContext {
-            saveDerivedContext(context)
-            return
-        }
-        context.perform {
+    // MARK: - Core Data Saving support
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
             do {
                 try context.save()
-            } catch let error as NSError {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
-        }
-    }
-    
-    public func saveDerivedContext(_ context: NSManagedObjectContext) {
-        context.perform {
-            do {
-              try context.save()
-            } catch let error as NSError {
-              fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-            self.saveContext(self.managedContext)
         }
     }
 }
