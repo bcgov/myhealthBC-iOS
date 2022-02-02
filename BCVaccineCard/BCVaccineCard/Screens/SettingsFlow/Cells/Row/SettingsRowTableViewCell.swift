f3d4b93f2890803a290d26a5ed4978be37c55c31
Mohamed Fawzy <mfawzy@freshworks.io>
2022-02-02 18:16:21 +0200

    set accessibilty trait for a label to button, because it recive tap action and navigate to another screen

--- BCVaccineCard/BCVaccineCard/Screens/SettingsFlow/Cells/Row/SettingsRowTableViewCell.swift
+++ BCVaccineCard/BCVaccineCard/Screens/SettingsFlow/Cells/Row/SettingsRowTableViewCell.swift
@@ -42,6 +42,7 @@ class SettingsRowTableViewCell: UITableViewCell, Theme {
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         self.contentView.gestureRecognizers?.removeAll()
         self.contentView.addGestureRecognizer(tap)
+        titleLabel.accessibilityTraits = .button
     }
     
     @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
