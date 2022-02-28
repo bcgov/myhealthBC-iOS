c711f92de61c628d0300ba23049c20147e2da946
Amir Shayegh <shayegh@me.com>
2022-02-28 06:52:12 -0800

    updated descypt script

--- .github/secrets/decrypt_firebase.sh
+++ .github/secrets/decrypt_firebase.sh
@@ -3,11 +3,11 @@
 set -eo pipefail
 
 gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS" --output ./.github/secrets/dist.p12 ./.github/secrets/dist.p12.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS" --output ./.github/secrets/prov2.mobileprovision ./.github/secrets/ClientVaccineCardADHOC.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$FW_KEYS" --output ./.github/secrets/ClientVaccineCard-ADHOC.mobileprovision ./.github/secrets/ClientVaccineCardADHOC.mobileprovision.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/prov2.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/prov2.mobileprovision
+cp ./.github/secrets/ClientVaccineCard-ADHOC.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/ClientVaccineCard-ADHOC.mobileprovision
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/dist.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
