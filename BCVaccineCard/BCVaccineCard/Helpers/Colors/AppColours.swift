49d3d3d4045638bf8fe54542b7708fb3744e2ca5
Amir Shayegh <shayegh@me.com>
2022-03-14 14:23:41 -0700

    cleanup

--- BCVaccineCard/BCVaccineCard/Helpers/Colors/AppColours.swift
+++ BCVaccineCard/BCVaccineCard/Helpers/Colors/AppColours.swift
@@ -22,7 +22,7 @@ class AppColours {
     static let borderGray = UIColor(red: 207/255, green: 207/255, blue: 207/255, alpha: 1.0)
     static let commentBackground = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1)
     static let commentDateTime = UIColor(red: 0.376, green: 0.376, blue: 0.376, alpha: 1)
-    static let divider = UIColor(red: 0.652, green: 0.751, blue: 0.85, alpha: 0.3)
+    static let divider = UIColor(red: 0.22, green: 0.349, blue: 0.541, alpha: 0.3)
     
     struct CovidTest {
         static let pendingText = appBlue
