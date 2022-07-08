e799de40ac20a9d7ccc8951552d5d84511fc315c
Amir Shayegh <shayegh@me.com>
2022-07-08 10:11:00 -0700

    -

---
+++ BCVaccineCard/BCVaccineCard/Data/HealthGatewayData.swift
@@ -0,0 +1,12 @@
+//
+//  HealthGatewayData.swift
+//  BCVaccineCard
+//
+//  Created by Amir on 2022-07-08.
+//
+
+import Foundation
+
+class HealthGatewayData: RemoteData {
+    
+}
---
+++ BCVaccineCard/BCVaccineCard/Data/RemoteData.swift
@@ -0,0 +1,13 @@
+//
+//  RemoteData.swift
+//  HealthGatewayTest
+//
+//  Created by Amir on 2022-07-08.
+//
+
+import Foundation
+import Alamofire
+
+class RemoteData {
+    
+}
