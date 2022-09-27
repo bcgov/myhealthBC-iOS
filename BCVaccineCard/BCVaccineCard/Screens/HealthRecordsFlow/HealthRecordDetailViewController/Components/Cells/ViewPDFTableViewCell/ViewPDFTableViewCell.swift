4311e6a81c0b05d2c632f05371ca67b02e2b0182
ConnorOgilvie91 <connor@freshworks.io>
2022-09-27 11:15:18 -0700

    Updated lab tests to include lab orders

---
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/HealthRecordDetailViewController/Components/Cells/ViewPDFTableViewCell/ViewPDFTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViewPDFTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2022-09-22.
+//
+
+import UIKit
+
+class ViewPDFTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
