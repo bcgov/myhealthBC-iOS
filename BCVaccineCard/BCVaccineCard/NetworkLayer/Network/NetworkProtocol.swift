36c1ae689fb132c89027d98b3bbdc0194e654c99
Amir Shayegh <shayegh@me.com>
2022-11-04 16:35:42 -0700

    added network extension for adding and removing loaders

--- BCVaccineCard/BCVaccineCard/NetworkLayer/Network/NetworkProtocol.swift
+++ BCVaccineCard/BCVaccineCard/NetworkLayer/Network/NetworkProtocol.swift
@@ -10,3 +10,21 @@ import Foundation
 protocol Network {
     func request<Parameters: Encodable, T: Decodable>(with requestData: NetworkRequest<Parameters, T>)
 }
+
+extension Network {
+    func addLoader(message: LoaderMessage) {
+        DispatchQueue.main.async {
+            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                appDelegate.incrementLoader(message: message)
+            }
+        }
+    }
+    
+    func removeLoader() {
+        DispatchQueue.main.async {
+            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                appDelegate.decrementLoader()
+            }
+        }
+    }
+}
