baa92f18a6694b56bc6b7bbb4b0ee122fcb01b22
Amir Shayegh <shayegh@me.com>
2022-11-24 15:06:44 -0800

    cleanup

--- BCVaccineCard/BCVaccineCard/NetworkLayer/Network/Services/MobileConfig/MobileConfigService.swift
+++ BCVaccineCard/BCVaccineCard/NetworkLayer/Network/Services/MobileConfig/MobileConfigService.swift
@@ -33,25 +33,4 @@ struct MobileConfigService {
             })
         network.request(with: request)
     }
-    
-    // Using hard coded auth configs
-    func fetchDefaultAuthConfig(completion: @escaping (MobileConfigurationResponseObject?)->Void) {
-        fetchConfig { remoteResponse in
-            guard let response = remoteResponse else {
-                return completion(nil)
-            }
-//            let modified = MobileConfigurationResponseObject(online: response.online, baseURL: response.baseURL, authentication: defaultAuth(), version: response.version)
-            return completion(response)
-        }
-    }
-    
-//    fileprivate func defaultAuth() -> AuthenticationConfig {
-//#if PROD
-//        return AuthenticationConfig(endpoint: "https://oidc.gov.bc.ca/auth/realms/ff09qn3f", identityProviderID: "bcsc2", clientID: "myhealthapp", redirectURI: "myhealthbc://*")
-//#elseif TEST
-//        return AuthenticationConfig(endpoint: "https://test.oidc.gov.bc.ca/auth/realms/ff09qn3f", identityProviderID: "bcsc", clientID: "myhealthapp", redirectURI: "myhealthbc://*")
-//#else
-//        return AuthenticationConfig(endpoint: "https://dev.oidc.gov.bc.ca/auth/realms/ff09qn3f", identityProviderID: "bcsc", clientID: "myhealthapp", redirectURI: "myhealthbc://*")
-//#endif
-//    }
 }
