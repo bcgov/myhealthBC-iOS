e60c12024c9c021b15c402b96ee65e105fece620
ConnorOgilvie91 <connor@freshworks.io>
2023-06-07 12:28:52 -0700

    Added placeholders for new VC's

---
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/NotesFlow/CreateNoteViewController/CreateNoteViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CreateNoteViewController.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-06-07.
+//
+
+import Foundation
---
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/NotesFlow/ViewDetailedNoteViewController/ViewDetailedNoteViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ViewDetailedNoteViewController.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-06-07.
+//
+
+import Foundation
