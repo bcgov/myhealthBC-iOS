34ff6b9be6ad0d7b5e38d546d64587bea7a3a63b
ConnorOgilvie91 <connor@freshworks.io>
2023-08-01 19:29:35 -0700

    Boilerplate tbvcell code added

---
+++ BCVaccineCard/BCVaccineCard/Screens/ManageHomeScreen/TableViewCells/ManageLinksTextCell/ManageLinksTextTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ManageLinksTextTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-08-01.
+//
+
+import UIKit
+
+class ManageLinksTextTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BCVaccineCard/BCVaccineCard/Screens/ManageHomeScreen/TableViewCells/ManageQuickLinksTVCell/ManageQuickLinkTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ManageQuickLinkTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-08-01.
+//
+
+import UIKit
+
+class ManageQuickLinkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
