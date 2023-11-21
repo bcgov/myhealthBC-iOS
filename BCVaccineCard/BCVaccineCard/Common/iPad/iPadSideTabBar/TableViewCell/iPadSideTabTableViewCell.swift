14bbaec342a13e99749485d2035b57b395e57c08
ConnorOgilvie91 <connor@freshworks.io>
2023-11-20 16:50:34 -0800

    Home screen placeholders

---
+++ BCVaccineCard/BCVaccineCard/Common/iPad/iPadSideTabBar/TableViewCell/iPadSideTabTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  iPadSideTabTableViewCell.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-11-20.
+//
+
+import UIKit
+
+class iPadSideTabTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BCVaccineCard/BCVaccineCard/Common/iPad/iPadSideTabBar/iPadSideTabTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  iPadSideTabTableViewController.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-11-20.
+//
+
+import UIKit
+
+class iPadSideTabTableViewController: UIViewController {
+    
+    @IBOutlet weak private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
