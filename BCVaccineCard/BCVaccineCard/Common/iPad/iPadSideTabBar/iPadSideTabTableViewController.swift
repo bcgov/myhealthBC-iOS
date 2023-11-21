c0f2c2116c52812ac9fb8f97c1cea7ee5775b95e
ConnorOgilvie91 <connor@freshworks.io>
2023-11-21 11:20:07 -0800

    First commit for 1673

--- BCVaccineCard/BCVaccineCard/Common/iPad/iPadSideTabBar/iPadSideTabTableViewController.swift
+++ BCVaccineCard/BCVaccineCard/Common/iPad/iPadSideTabBar/iPadSideTabTableViewController.swift
@@ -14,11 +14,6 @@ class iPadSideTabTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
 }
