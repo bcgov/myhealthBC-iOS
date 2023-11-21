033f930b3eb8e66c4ff0015862679fe51942d6f8
ConnorOgilvie91 <connor@freshworks.io>
2023-11-21 12:54:27 -0800

    Placeholder files added

---
+++ BCVaccineCard/BCVaccineCard/Common/iPad/SplitViewControllers/ReusableSplitViewController/ReusableSplitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReusableSplitViewController.swift
+//  BCVaccineCard
+//
+//  Created by Connor Ogilvie on 2023-11-21.
+//
+
+import UIKit
+
+class ReusableSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
