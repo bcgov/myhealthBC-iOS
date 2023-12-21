6ab370baeb00966908817db5272f9ec34ff4bcaa
Connor <connor.ogilvie@ca.ey.com>
2023-12-20 21:14:10 -0800

    Added child - still need to do add child vc

--- BCVaccineCard/BCVaccineCard/Common/iPad/SplitViewControllers/AlertStyleUI/iPadAlertViewController/iPadAlertViewController.swift
+++ BCVaccineCard/BCVaccineCard/Common/iPad/SplitViewControllers/AlertStyleUI/iPadAlertViewController/iPadAlertViewController.swift
@@ -1,8 +1,34 @@
 //
-//  iPadAlertView.swift
+//  iPadAlertViewController.swift
 //  BCVaccineCard
 //
 //  Created by Connor Ogilvie on 2023-12-15.
 //
 
-import Foundation
+import UIKit
+
+class iPadAlertViewController: UIViewController {
+    
+    class func construct(with splitVC: iPadAlertSplitVC) -> iPadAlertViewController {
+        var vc = iPadAlertViewController()
+        vc.splitVC = splitVC
+        return vc
+    }
+    
+    private weak var splitVC: iPadAlertSplitVC?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        self.view.backgroundColor = AppColours.backgroundGray
+        addSplitVCToSelf()
+    }
+    
+    private func addSplitVCToSelf() {
+        guard let splitVC = splitVC else { return }
+        self.view.addSubview(splitVC.view)
+    }
+}
