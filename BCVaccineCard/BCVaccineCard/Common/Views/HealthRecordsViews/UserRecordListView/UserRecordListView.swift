d7ac9791730dc0647aca2b5b0e5e99f432238a8c
Connor <connor.ogilvie@ca.ey.com>
2024-02-13 14:19:30 -0800

    Removed date from clinical docs and hospital visits again

--- BCVaccineCard/BCVaccineCard/Common/Views/HealthRecordsViews/UserRecordListView/UserRecordListView.swift
+++ BCVaccineCard/BCVaccineCard/Common/Views/HealthRecordsViews/UserRecordListView/UserRecordListView.swift
@@ -104,10 +104,10 @@ class UserRecordListView: UIView {
         }
         
         switch record.type {
-//        case .hospitalVisit, .clinicalDocument:
-//            // records that dont show date
-//            recordTypeSubtitleLabel.text = statusToInclude
-        case .covidImmunizationRecord, .covidTestResultRecord, .medication, .healthVisit, .specialAuthorityDrug, .laboratoryOrder, .immunization, .diagnosticImaging, .cancerScreening, .note, .hospitalVisit, .clinicalDocument:
+        case .hospitalVisit, .clinicalDocument:
+            // records that dont show date
+            recordTypeSubtitleLabel.text = statusToInclude
+        case .covidImmunizationRecord, .covidTestResultRecord, .medication, .healthVisit, .specialAuthorityDrug, .laboratoryOrder, .immunization, .diagnosticImaging, .cancerScreening, .note:
             // records that show date
             var text: String
             if let statusToInclude = statusToInclude, statusToInclude.trimWhiteSpacesAndNewLines.count > 0 {
