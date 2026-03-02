4088633a890eff7c2d79e72d7d85ce9324c4f219
Amir Shayegh <amir.shayegh@ca.ey.com>
2026-03-02 15:30:39 -0800

    Update BC Cancer screening text to support Breast, Colon, Lung, and Cervix programs

--- BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/HealthRecordDetailViewController/Components/CancerScreening/CancerScreeningDetailView.swift
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/HealthRecordDetailViewController/Components/CancerScreening/CancerScreeningDetailView.swift
@@ -163,8 +163,19 @@ extension CancerScreeningDetailView {
         switch model.type {
         case .cancerScreening(model: let model):
            
-            let firstText = model.eventType == "Result" ? "Download your cervix screening result letter. It may include important information about next steps. If you have questions, check the BC Cancer website or talk to your care provider." : "Find out about your cervix screening next steps. You will also get this letter in the mail. Learn more about cervix screening."
-            let linkedStrings = model.eventType == "Result" ? LinkedStrings(text: "check the BC Cancer website", link: "http://www.bccancer.bc.ca/screening/cervix", underlined: true) : LinkedStrings(text: "Learn more about cervix screening", link: "http://www.bccancer.bc.ca/screening/cervix", underlined: true)
+            var programName = (model.programName ?? "cervix")
+                .components(separatedBy: " ")
+                .first?
+                .lowercased() ?? "cervix"
+            if programName == "cervical" { programName = "cervix" }
+
+            let baseScreeningURL = "http://www.bccancer.bc.ca/screening/"
+            let firstText = model.eventType == "Result"
+                ? "Download your \(programName) screening result letter. It may include important information about next steps. If you have questions, check the BC Cancer website or talk to your care provider."
+                : "Find out about your \(programName) screening next steps. You will also get this letter in the mail. Learn more about \(programName) screening."
+            let linkedStrings = model.eventType == "Result"
+                ? LinkedStrings(text: "check the BC Cancer website", link: "\(baseScreeningURL)\(programName)", underlined: true)
+                : LinkedStrings(text: "Learn more about \(programName) screening", link: "\(baseScreeningURL)\(programName)", underlined: true)
             let links = [linkedStrings]
                         
             let fields: [TextListModel] = [
--- BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/UsersListOfRecordsViewController/Views/HealthRecordsLearnMoreView/HealthRecordsLearnMoreView.swift
+++ BCVaccineCard/BCVaccineCard/Screens/HealthRecordsFlow/UsersListOfRecordsViewController/Views/HealthRecordsLearnMoreView/HealthRecordsLearnMoreView.swift
@@ -65,8 +65,8 @@ class HealthRecordsLearnMoreView: UIView {
     }
     
     private func setupForBCCancer(attributes: [NSAttributedString.Key: Any]) -> NSMutableAttributedString {
-        let attrStr = NSMutableAttributedString(string: "Only BC Cancer cervix screening letters are available here. Your Health Gateway timeline may include these and other screening test results in lab or imaging reports. Learn more", attributes: attributes)
-        if let range = attrStr.range(textToFind: "Learn more"), let url = URL(string: "http://www.bccancer.bc.ca/screening") {
+        let attrStr = NSMutableAttributedString(string: "BC Cancer screening letters for Breast, Colon, Lung, and Cervix are available here. Your Health Gateway timeline may include these and other screening test results in lab or imaging reports. Learn more", attributes: attributes)
+        if let range = attrStr.range(textToFind: "Learn more"), let url = URL(string: "https://www2.gov.bc.ca/gov/content/health/managing-your-health/health-gateway/guide/healthrecords") {
             let attr: [NSAttributedString.Key: Any] = [
                 NSAttributedString.Key.link: url
             ]
